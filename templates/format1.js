451e1542d4e73be16d544b787373bc4dfaf2628a
Alyphrius <brokenflames909@gmail.com>
2025-08-06 00:51:37 -0500

    Delete templates/format1.js

--- templates/format1.js
+++
@@ -1,8 +0,0 @@
-
-
-fetch("/").then(response => (response.text())).then(htmlstring =>{
-    const parser=new DOMParser()
-    const mdoc=parser.parseFromString(htmlstring,'text/html')
-    const tbclone=mdoc.getElementById("topbar").cloneNode(true)
-    document.body.insertBefore(tbclone,document.body.firstChild)
-})
