4213edc05f8b075f4305c5fd48b2753b477c2975
Alyphrius <brokenflames909@gmail.com>
2025-08-06 00:54:26 -0500

    Update audiorun.js

--- static/audiorun.js
+++ static/audiorun.js
@@ -1,9 +1,11 @@
 
 import {keyfrequency} from './scripts/client1.js'
+import {module as emodule} from './effect_modules.js';
 
 let mintime=0
 let maxtime=0
 let gsegments=[]
+const globalvars={"F":`440`,"smt":"2**(1/12)"}
 
 
 function gpt_calcgraphval(x, segments) {
@@ -38,6 +40,9 @@ function gpt_calcgraphval(x, segments) {
 }
 
 function parsemath(exp, localvars, lvarstack = [], returnfloat) {
+
+
+
     if (typeof exp !== 'string') exp = String(exp);
 
     if (exp.slice(0,2)=="g!") {
@@ -81,8 +86,43 @@ function parsemath(exp, localvars, lvarstack = [], returnfloat) {
     
 }
 
+function expandchain(exp,localvars,effects,lvarstack=[],rawch=[]) {
+    //console.log(effects)
+    if (exp.slice(0,2)==="e!") {
+        exp=exp.slice(2);
+        const nodes=exp.split("-");
+        for (let n of nodes) {
+            if (n.charAt(0)==="$" && n.charAt(n.length-1)==="#") {
+                for (let c of getrawchain(localvars[n.slice(1,n.length-1)],lvarstack,rawch)) {
+                    rawch.push(effects[c])
+                }
+            } else {
+                rawch.push(effects[n])
+                console.log("fek: ",effects[n])
+            }
+        }
+        //console.log(rawch)
+        return rawch;
+    } else {
+        console.warn("Invalid effect chain");
+        
+        return;
 
+    }
 
+}
+function getrawchain(exp,localvars,effects) {
+    const chain=expandchain(exp,localvars,effects,[],[])
+    
+    // for (let e of chain) {
+    //     for (let p in e.params) {
+    //         if (e.params.hasOwnProperty(p)) {
+    //             e.params[p]=Number(parsemath(e.params[p],localvars,[]))
+    //         }
+    //     }
+    // }
+    return chain
+}
 
 
 
@@ -92,14 +132,14 @@ function cartesarrays(arrays) {
     return acc.flatMap(a => curr.map(b => [...a, b]));
   }, [[]]);
 }
-function getrawwaves(t,variables,waveformdata) {
-    const newvars={...variables,...{"t":`${String(t)}`,"F":`${String(keyfrequency[0])}`}}
+function getrawwaves(t,variables,waveformdata,effects) {
+    const newvars={...variables,...{"t":`${String(t)}`},...globalvars}
 
     // if (playing==false) {
     //     return {};
     // }
 
-
+    let idx=0
     let newdata=[];
     //console.log(waveformdata)
     waveformdata.forEach((item)=>{
@@ -137,6 +177,10 @@ function getrawwaves(t,variables,waveformdata) {
                 wavetab.amplitude=Number(parsemath(item.amplitude,finvars,[]));
                 wavetab.phase=Number(parsemath(item.phase,finvars,[]));
                 wavetab.pan=Number(parsemath(item.pan,finvars,[]))
+                //wavetab.fxchain=getrawchain(item.fxchain,finvars,effects);
+                wavetab.mixtrack=item.mixtrack;
+                wavetab.idx=idx
+                idx+=1
                 newdata.push(wavetab)
 
             });
@@ -146,7 +190,11 @@ function getrawwaves(t,variables,waveformdata) {
           wavetab.frequency=Number(parsemath(item.frequency,newvars,[]));
           wavetab.amplitude=Number(parsemath(item.amplitude,newvars,[]));
           wavetab.phase=Number(parsemath(item.phase,newvars,[]));
-          wavetab.pan=Number(parsemath(item.pan,newvars,[]))
+          wavetab.pan=Number(parsemath(item.pan,newvars,[]));
+          //wavetab.fxchain=getrawchain(item.fxchain,variables,effects);
+          wavetab.mixtrack=item.mixtrack;
+          wavetab.idx=idx;
+          idx+=1
           newdata.push(wavetab)
         }
       
@@ -155,8 +203,24 @@ function getrawwaves(t,variables,waveformdata) {
     return newdata
 }
 
+function getmixerdata(rawwaves) {
+    const dat={}
+    for (let w of rawwaves) {
+        if (dat.hasOwnProperty(w.mixtrack)) {
+            dat[w.mixtrack].push(w)
+        } else {
+            dat[w.mixtrack]=[w]
+        }
+    }
+    return dat;
 
-export function generateAudioBuffer(context, startTime, endTime, waveformdata,variables,blocksize,phases=[],segments,sampleRate = 44100) {
+}
+
+
+export function generateAudioBuffer(context, startTime, endTime, waveformdata,variables,blocksize,phases=[],effects,mixtracks,segments,sampleRate = 44100) {
+    console.log("got effects as ",effects)
+    
+    globalvars.F=`${String(keyfrequency[0])}`
   const duration = endTime - startTime;
   const frameCount = duration * sampleRate;
   const buffer = context.createBuffer(2, frameCount, sampleRate);
@@ -166,72 +230,133 @@ export function generateAudioBuffer(context, startTime, endTime, waveformdata,va
   maxtime=endTime;
   gsegments=segments;
   
- let waves=null;   
- 
+    let waves=null;   
+    let rwaves=null;
+    const mixersamples={}
+    console.log("effects first reg genaudio: ",effects)
   for (let i = 0; i < frameCount; i++) {
     const t = startTime + i / sampleRate;
 
   
     
     if (i%blocksize==0) {
-        waves=getrawwaves(t,variables,waveformdata);
+        rwaves=getrawwaves(t,variables,waveformdata,effects);
+        waves=getmixerdata(rwaves)
         //console.log(waves)
 
     }
-    let lsampleval=0
-    let rsampleval=0
+    if (i==0) {
+        console.log("waves: ")
+        console.log(waves)
+        for (let h in waves) {
+            mixersamples[h]=[[],[]]
+        }
+    }
+   
+    
    // console.log(waves)
     
     if (!waves) {continue;}
-    if (phases.length !== waves.length) {
+    if (phases.length !== rwaves.length) {
         phases.length=0;
-        for (let i = 0; i < waves.length; i++) {
+        for (let i = 0; i < rwaves.length; i++) {
             phases.push(0)
         }
     }
-    for (let j = 0; j < waves.length; j++) {
-    
-        const { waveform, frequency, amplitude, pan,phase } = waves[j];
-        //console.log(waves[j])
-        const inc = 2 * Math.PI * frequency / sampleRate;
-        phases[j] += inc;
-        if (phases[j] > 2 * Math.PI) {
-            phases[j] -= 2 * Math.PI;
-        }
-        const totphase = phases[j] + phase;
+    //console.log(phases)
+    for (let wi in waves) {
+        const wav=waves[wi]
+        let lsampleval=0
+        let rsampleval=0
         
-        let val = 0;
-        switch (waveform) {
-            case "sine":
-                val = Math.sin(totphase);
-                break;
-            case "square":
-                val = Math.sign(Math.sin(totphase));
-                break;
-            case "sawtooth":
-                val = 2 * (totphase / (2 * Math.PI) - Math.floor(totphase / (2 * Math.PI) + 0.5));
-                break;
-            case "triangle":
-                val = 2 * Math.abs(2 * (totphase / (2 * Math.PI) - Math.floor(totphase / Math.PI + 0.5))) - 1;
-                break;
-            case "whitenoise":
-                val=-1+2*Math.random();
-        }
+        for (let j = 0; j < wav.length; j++) {
+        
+            const { waveform, frequency, amplitude, pan,phase, mixtrack,idx} = wav[j];
 
-        lsampleval+=amplitude * val*(1-(pan+1)/2);
-        rsampleval+=amplitude * val*((pan+1)/2)
-        phases[j] = totphase
-        if (phases[j] > 2 * Math.PI) phases[j] -= 2 * Math.PI;
-        
+            
+            //console.log(waves[j])
+            const inc = 2 * Math.PI * frequency / sampleRate;
+            phases[idx] += inc;
+            if (phases[idx] > 2 * Math.PI) {
+                phases[idx] -= 2 * Math.PI;
+            }
+            const totphase = phases[idx] + phase;
+            
+            let val = 0;
+            //console.log(waveform,frequency,amplitude,pan,phase,idx,totphase)
+            switch (waveform) {
+                case "sine":
+                    val = Math.sin(totphase);
+                    break;
+                case "square":
+                    val = Math.sign(Math.sin(totphase));
+                    break;
+                case "sawtooth":
+                    val = 2 * (totphase / (2 * Math.PI) - Math.floor(totphase / (2 * Math.PI) + 0.5));
+                    break;
+                case "triangle":
+                    val = 2 * Math.abs(2 * (totphase / (2 * Math.PI) - Math.floor(totphase / Math.PI + 0.5))) - 1;
+                    break;
+                case "whitenoise":
+                    val=-1+2*Math.random();
+            }
+            let tsvl=amplitude * val*(1-(pan+1)/2);
+            let tsvr=amplitude * val*((pan+1)/2);
+            //console.log(fxchain)
+            // for (let e of fxchain) {
+            //     const ta=emodule.effects[e.type]([[tsvl],[tsvr]],e.params)
+            //     tsvl=ta[0][0];
+            //     tsvr=ta[1][0];
+            // }
+            lsampleval+=tsvl;
+            rsampleval+=tsvr;
+            //console.log(rsampleval)
+            phases[idx] = totphase
+            if (phases[idx] > 2 * Math.PI) phases[idx] -= 2 * Math.PI;
+            
+        }
+        mixersamples[wi][0].push(lsampleval)
+        mixersamples[wi][1].push(rsampleval);
     }
+    
 
-    ldata[i] = lsampleval
-    rdata[i]=rsampleval
-   
+    
     
   }
+  const fl=[]
+  const fr=[]
+  console.log("mixersamples:")
+  console.log(mixersamples)
+  for (let h in mixersamples) {
+    if (mixtracks[h].slice(0,2)==="e!") {
+        console.log("chain stuff befre getrawchain: ",mixtracks[h],effects)
+    const chain=getrawchain(mixtracks[h],variables,effects);
+    console.log("chain stuff: ",chain)
+    for (let e of chain) {
+        
+        mixersamples[h]=emodule.effects[e.type](mixersamples[h],e.params,startTime,sampleRate,blocksize)
+    }
+    }
+    
+  }
+
+  for (let h in mixersamples) {
+    for (let i=0;i<mixersamples[h][0].length;i++) {
+        ldata[i]+=mixersamples[h][0][i]
+    }
+    for (let i=0;i<mixersamples[h][1].length;i++) {
+        rdata[i]+=mixersamples[h][1][i]
+    }
+    
+  }
+
+//   ldata[i] = lsampleval
+//     rdata[i]=rsampleval
+   
   //console.log(data)
-  //console.log(buffer.getChannelData(0))
+ console.log(buffer.getChannelData(0))
+  
   return [buffer,phases];
 }
 
+export {parsemath,globalvars}
